eb2356183295346fdd647617e89ee9f970702001
arnaud <arnaud.boidard@gmail.com>
2021-08-21 17:51:10 +0200

    implementation hook items

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const cors = require('cors');
+
 const app = express()
 
 const port = process.env.SERVER_PORT
@@ -6,6 +8,14 @@ const port = process.env.SERVER_PORT
 console.log("running NODE_ENV : " + process.env.NODE_ENV)
 
 app.use(express.json())
+
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
 
 app.get('/version', (req, res) => {
     console.log("request version")
