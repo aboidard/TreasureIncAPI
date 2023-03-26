24ce333ee11d1f63248c373d35f03bd09566d22b
arnaud <arnaud.boidard@gmail.com>
2023-03-26 16:20:19 +0200

    fix docker

--- routes/V1/expeditions.js
+++ routes/V1/expeditions.js
@@ -1,7 +1,7 @@
 import express from 'express'
 
 import logger from '../../config/logger'
-import Expedition from '../../models/Expedition'
+import Expedition from '../../models/expedition'
 
 const router = express.Router();
 
